894d380fe6d3602fb58e3d22d825353b36aac2ba
diego <diego@DESKTOP-VC77R42.localdomain>
2023-03-02 21:11:22 -0500

    Web-requests and Pandas

--- app/main.py
+++ app/main.py
@@ -2,19 +2,27 @@ import utils
 import random
 import read_csv
 import charts
+import pandas as pd
 
 
 
 def run():
-
+    '''
     data = read_csv.read_csv('./app/data.csv')
     data = list(filter(lambda item : item['Continent'] =='South America',data))
         
     countries = list(map(lambda x: x['Country/Territory'],data))
     percentages = list(map(lambda x: x['World Population Percentage'],data))
     charts.generate_pie_chart(countries, percentages)
+    '''
+    df = pd.read_csv('./app/data.csv')
+    df = df[df['Continent']== 'South America']
+    countries = df['Country/Territory'].values
+    percentages = df['World Population Percentage'].values
 
+    charts.generate_pie_chart(countries, percentages)
 
+    data = read_csv.read_csv('./app/data.csv')
     country = input('Type Country => ')
     result = utils.population_by_country(data, country)
 
